83243c4b1a3d4058bf8856e14ef2de2a85562fd2
Eric Sherman <ericandrewsherman@gmail.com>
2021-03-26 11:31:02 -0500

    dependency and style tweaks

--- .babelrc.js
+++ .babelrc.js
@@ -6,11 +6,10 @@ module.exports = {
                 modules: false,
             },
         ],
-        '@babel/preset-react',
     ],
     env: {
         test: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ['@babel/preset-env'],
         },
     },
 };
