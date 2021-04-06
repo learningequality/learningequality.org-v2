8ced249b932146a489b648842ec79da011874122
Eric Sherman <ericandrewsherman@gmail.com>
2021-04-05 19:24:56 -0500

    two different ways to load types

---
+++ learning_equality/people/choices.py
@@ -0,0 +1,16 @@
+from django.db.models import TextChoices
+
+# Field choice Enumeration types
+# see https://docs.djangoproject.com/en/3.0/releases/3.0/#enumerations-for-model-field-choices
+class PersonType(TextChoices):
+    """
+    categories for team members
+    """
+
+    CURRICULUM = "curriculum", "Curriculum & Pedagogy"
+    IMPLEMENTATIONs = "implementations", "Program Implementations"
+    PARTNERSHIPS = "partnerships", "Partnerships"
+    ENGINEERING = "engineering", "Engineering"
+    UX = "UX", "UX Design & Research"
+    COMMUNICATIONS = "communications", "Communications"
+    OPERATIONS = "operations", "Team Operations"
